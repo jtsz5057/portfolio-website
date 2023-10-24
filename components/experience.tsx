24afd5a136cd2681eef13619202489a3e26135df
Jonathan Tan <jtsz5057@gmail.com>
2023-10-24 22:45:25 +1000

    create experience component and install react vertical timeline component

---
+++ components/experience.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+export default function Experience() {
+  return (
+    <section>
+        <h2 className="text-3xl font-medium mb-8">About Me</h2>
+        <VerticalTimeline>
+            
+        </VerticalTimeline>
+
+    </section>
+  )
+}
