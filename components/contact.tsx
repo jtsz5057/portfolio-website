5bbbf511b2c007700a726fa78100e193abfc4f00
Jonathan Tan <jtsz5057@gmail.com>
2023-10-24 10:34:58 +1000

    create contact component.

---
+++ components/contact.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <section>
+        <h2 className="text-3xl font-medium mb-8">Contact Me</h2>
+        <p>Please contact me directly at email or through this form.</p>
+    </section >
+  )
+}
