729e3152675f97776cf121fc9ae6edb43f789be7
Jonathan Tan <jtsz5057@gmail.com>
2023-10-24 23:53:56 +1000

    add the section of experience

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import SectionDivider from '@/components/section-divider'
 import About from '@/components/about'
 import Skills from '@/components/skills'
 import Contact from '@/components/contact'
+import Experience from '@/components/experience'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <SectionDivider/>
       <About/>
       <Skills/>
+      <Experience/>
       <Contact/>
     </main>
   )
--- components/experience.tsx
+++ components/experience.tsx
@@ -1,13 +1,27 @@
+'use client';
+
 import React from 'react'
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement, }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import { experiencesData } from '../lib/data';
 
 export default function Experience() {
   return (
     <section>
-        <h2 className="text-3xl font-medium mb-8">About Me</h2>
-        <VerticalTimeline>
-            
+        <h2 className="text-3xl font-medium mb-8">My Experience</h2>
+        <VerticalTimeline
+            contentStyle={{}}>
+            {
+                experiencesData.map((item, index) => (
+                    <React.Fragment key={index}>
+                        <VerticalTimelineElement>
+                            <h3>{item.title}</h3>
+                            <p>{item.location}</p>
+                            <p>{item.description}</p>
+                        </VerticalTimelineElement>
+                    </React.Fragment>
+                ))
+            }
         </VerticalTimeline>
 
     </section>
