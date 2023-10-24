a687eeaf14b1142e4a142e91fa6faeca5aa86b9b
Jonathan Tan <jtsz5057@gmail.com>
2023-10-24 13:19:55 +1000

    create and modified contact companonent and page

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
+
 export default function Contact() {
   return (
-    <section>
+    <section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40">
         <h2 className="text-3xl font-medium mb-8">Contact Me</h2>
-        <p>Please contact me directly at email 
-            <a className="underline" href="mailto:jtsz5057@gmail.com">jtsz5057@gmail.com</a>{''}
-            or through this form.</p>
+        <p className="text-gray-700">Please contact me directly at {''} 
+            <a className="underline" href="mailto:jtsz5057@gmail.com">jtsz5057@gmail.com {''}</a>
+            .</p>
     </section >
   )
 }
